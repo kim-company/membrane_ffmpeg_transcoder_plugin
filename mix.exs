34953f14e2f40ca573225044ab638f120e064f9e
Daniel Morandini <danielmorandini@me.com>
2025-11-06 12:18:06 +0100

    add tminig information to muxed buffers

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Membrane.FFmpeg.Transcoder.MixProject do
   def project do
     [
       app: :membrane_ffmpeg_transcoder_plugin,
-      version: "1.5.9",
+      version: "1.5.10",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       source_url: @github_url,
